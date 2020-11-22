b42f678ee9f6101969c3165edbb85c234fa91785
samuelClo <minecraft.cloarec@hotmail.fr>
2020-11-22 16:50:52 +0100

    first commit

---
+++ database/migrations/2020_11_22_153438_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('city_id');
+            $table->foreignId('category_id');
+            $table->foreignId('sub_category_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('description')->nullable();
+            $table->boolean('is_published')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
